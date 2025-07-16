d7ded02370df262acc0ccb3f7cb72ce708bf5be2
Abhishek <abhishekroy2342005@gmail.com>
2025-07-16 21:30:50 +0530

    Clean initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense,useState, useEffect } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Test from '../pages/Test'; 
@@ -6,9 +6,9 @@ import { Header } from '../components/Header/Header';
 import Auth from '../pages/Auth'; 
 import Community from '../pages/Community';  
 import Resources from '../pages/Resources'; 
-import ChatWindow  from '../components/Chatbot/ChatWindow';
+import ChatWindow from '../components/Chatbot/ChatWindow';
 import { onAuthStateChanged } from 'firebase/auth';
-import MoodDashboard from '../pages/MoodTracker'
+import MoodDashboard from '../pages/MoodTracker';
 import { auth } from '../context/firebase/firebase';
 import PrivacyPolicy from '../pages/PrivacyPolicy';
 import CookiePolicy from '../pages/CookiePolicy';
@@ -19,7 +19,6 @@ import MentalWellnessResources from '../pages/WellnessResources';
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [checkingAuth, setCheckingAuth] = useState(true);
-
   const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
@@ -32,7 +31,7 @@ function App() {
       setCheckingAuth(false);
     });
 
-    return () => unsubscribe(); // Cleanup listener on unmount
+    return () => unsubscribe();
   }, []);
 
   if (checkingAuth) {
@@ -53,21 +52,28 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/test" element={<Test />} />
             <Route path="/therapies" element={<MoodDashboard user={currentUser} />} />
-            <Route path='/auth' element={<Auth/>} />
-            <Route path='/community' element={<Community/>} />
-            <Route path="/resources" element={<Resources/>} />
-            <Route path="/resourcess" element={<MentalWellnessResources/>} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/resourcess" element={<MentalWellnessResources />} />
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/cookie-policy" element={<CookiePolicy />} />
             <Route path="/terms-of-service" element={<TermsOfService />} />
-            
-            if (currentUser){currentUser && (
-              <Route path="/chatbot" element={ <ChatWindow user={currentUser} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} />
-            )}
-            else{
-              <Route path="/auth" element={<Auth />} />
-            }
 
+            {currentUser ? (
+              <Route
+                path="/chatbot"
+                element={
+                  <ChatWindow
+                    user={currentUser}
+                    darkMode={darkMode}
+                    toggleDarkMode={toggleDarkMode}
+                  />
+                }
+              />
+            ) : (
+              <Route path="/chatbot" element={<Auth />} />
+            )}
           </Routes>
         </Suspense>
       </main>
