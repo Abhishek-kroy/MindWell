b8f7039ca72ecb5b80a6e027c9f26d77f041e56e
Abhishek <abhishekroy2342005@gmail.com>
2025-07-29 11:54:43 +0530

    Resources added

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,6 @@ function App() {
     setDarkMode(!darkMode);
   };
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      setCheckingAuth(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (checkingAuth) {
-    return (
-      <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-900 via-blue-400 to-black text-white">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-400"></div>
-        <p className="mt-4 text-lg font-medium animate-pulse">Checking authentication...</p>
-      </div>
-    );
-  }
-
   return (
     <Router>
       <Header />
